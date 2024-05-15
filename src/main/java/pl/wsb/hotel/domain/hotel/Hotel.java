45b7ff5f375f3295738fdfe9b0595ae12451d2c6
gencieply <102959033+gencieply@users.noreply.github.com>
2024-05-15 21:32:31 +0200

    Ja jestem Fagata sexy cipka z pod Konina

--- src/main/java/pl/wsb/hotel/domain/hotel/Hotel.java
+++ src/main/java/pl/wsb/hotel/domain/hotel/Hotel.java
@@ -1,12 +1,17 @@
 package pl.wsb.hotel.domain.hotel;
 
 import pl.wsb.hotel.domain.client.Client;
+import pl.wsb.hotel.domain.client.exception.ClientNotFoundException;
 import pl.wsb.hotel.domain.hotel.room.Room;
 import pl.wsb.hotel.domain.hotel.room.RoomReservation;
+import pl.wsb.hotel.domain.hotel.room.exception.ReservationNotFoundException;
+import pl.wsb.hotel.domain.hotel.room.exception.RoomNotFoundException;
+import pl.wsb.hotel.domain.hotel.room.exception.RoomReservedException;
 import pl.wsb.hotel.domain.hotel.service.AbstractService;
-
+import pl.wsb.hotel.domain.hotel.HotelCapability;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Hotel implements HotelCapability{
@@ -53,6 +58,64 @@ public class Hotel implements HotelCapability{
             .orElse(null);
     }
 
+    @Override
+    public int getNumberOfUnderageClients() {
+        try {
+            return this.clients.stream()
+                    .filter(client -> {
+                        try {
+                            return client.getAge()<18;
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .findFirst()
+                    .orElse(null).getAge();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String addRoom(double area, int floor, boolean hasKingSizeBed, String description) {
+        return null;
+    }
+
+    @Override
+    public double getRoomArea(String roomId) {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfRoomsWithKingSizeBed(int floor) {
+        return 0;
+    }
+
+    @Override
+    public String addNewReservation(String clientId, String roomId, LocalDate date) throws ClientNotFoundException, RoomNotFoundException, RoomReservedException {
+        return null;
+    }
+
+    @Override
+    public String confirmReservation(String reservationId) throws ReservationNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean isRoomReserved(String roomId, LocalDate date) throws RoomNotFoundException {
+        return false;
+    }
+
+    @Override
+    public int getNumberOfUnconfirmedReservation(LocalDate date) {
+        return 0;
+    }
+
+    @Override
+    public Collection<String> getRoomIdsReservedByClient(String clientId) throws ClientNotFoundException {
+        return null;
+    }
+
     public String addClient(String firstName, String lastName, LocalDate birthDate) {
         if(this.clients == null) {
             this.clients = new ArrayList<>();
@@ -64,6 +127,7 @@ public class Hotel implements HotelCapability{
         this.clients.add(client);
         return client.getId();
     }
+
 
     public List<RoomReservation> getReservations() {
         return reservations;
@@ -100,4 +164,5 @@ public class Hotel implements HotelCapability{
     public void setRooms(List<Room> rooms) {
         this.rooms = rooms;
     }
+
 }
